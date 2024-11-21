af97204c9b7f7f4827329029a03a836ea7b92d69
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2024-11-20 17:04:47 -0700

    Moved SQL statements into functions and created a function to read in employee records

--- main.py
+++ main.py
@@ -1,20 +1,63 @@
 import sqlite3
-import datetime
-import os
+#import os
 
-conn = sqlite3.connect('database.db')
+def read_employee_records():
+    document = []
+    file = open('employee_data.csv', 'r')
+    # print(file)
+    i = 0
+    for line in file:
+        if i == 0:
+            pass
+        else:
+            temp = line.split(',', 5)
+            temp2 = tuple(temp)
 
-cursor = conn.cursor()
+            teammember_ID = temp[0]
+            name = temp2[1]
+            hourly_wage = temp[2]
+            opening_closing = temp[3]
+            fulltime_partime = temp[4]
+            hire_date = temp2[5]
 
-# res = cursor.execute("CREATE TABLE lol (teammember_ID, name, hourly_wage, opening_closing, fulltime_parttime, hire_date)")
+            cleaned_line = (int(teammember_ID), name, float(hourly_wage), int(opening_closing), int(fulltime_partime), int(hire_date[7:]))
+            document.append(cleaned_line)
+        
+        i += 1
+    return document
 
-# res = cursor.execute("""INSERT INTO Primary_database VALUES (514926, "John Doe", 16.50, 0, 0, 2019)""")
+def select(cursor):
+    res = cursor.execute('SELECT * FROM employees WHERE hourly_wage = 22.22')
+    print(res.fetchall())
 
-res = cursor.execute('SELECT * FROM Primary_database')
-# conn.commit()
-# print(res)
-print(res.fetchone())
+def create_table(cursor):
+    cursor.execute('CREATE TABLE employees (teammember_ID integer PRIMARY KEY NOT NULL, name text, hourly_wage real, opening_closing integer, fulltime_parttime integer, hire_date integer)')
 
-# print(rows)
+def initialize(cursor, document):
+    cursor.executemany('INSERT INTO employees VALUES (?, ?, ?, ?, ?, ?)', document) 
 
-# conn.close()
+def delete_record(cursor, unique_ID):
+    cursor.execute("""DELETE from employees where teammember_ID = ?""", unique_ID)
+
+def insert_record(cursor):
+    cursor.execute('INSERT INTO employees VALUES (NULL, "do", 22.22, 0, 0, 20200304)')
+
+def main():
+    # Schema: (514926, "John Doe", 16.50, 0, 0, "20200228"), date schema: yyyymmdd
+    conn = sqlite3.connect('toy.db')
+    cursor = conn.cursor()
+
+    create_table(cursor)
+    document = read_employee_records()
+    initialize(cursor, document)
+
+    # insert_record(cursor)
+    # select(cursor)
+    
+    conn.commit()
+    conn.close()
+
+try:
+    main()
+except Exception as e:
+    print(e, "Check file for missing or incorrect entries!")
