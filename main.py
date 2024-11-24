8a25da7ca446169b65f2c5c575aa99580841b489
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2024-11-23 17:33:44 -0700

    Added availability table logic

--- main.py
+++ main.py
@@ -26,15 +26,40 @@ def read_employee_records():
         i += 1
     return document
 
-def select(cursor):
-    res = cursor.execute('SELECT * FROM employees WHERE hourly_wage = 22.22')
+def read_availability_records():
+    document = []
+    file = open('availability.csv', 'r')
+    i = 0
+    for line in file:
+        if i == 0:
+            pass
+        else:
+            temp = line.split(',', 3)
+            temp2 = tuple(temp)
+
+            teammember_ID = temp2[0]
+            day_of_the_week = temp2[1]
+            start_time = temp2[2]
+            end_time = temp2[3]
+
+            cleaned_line = (int(teammember_ID), day_of_the_week, int(start_time), int(end_time))
+            document.append(cleaned_line)
+        i += 1
+    return document
+
+def select(cursor, name_of_table, constraints):
+    statement = "SELECT * FROM {tablename} WHERE {condition}".format(tablename = name_of_table, condition=constraints)
+    res = cursor.execute(statement)
     print(res.fetchall())
 
 def create_table(cursor):
     cursor.execute('CREATE TABLE employees (teammember_ID integer PRIMARY KEY NOT NULL, name text, hourly_wage real, opening_closing integer, fulltime_parttime integer, hire_date integer)')
 
+def create_availability_table(cursor):
+    cursor.execute('CREATE TABLE availability (teammember_ID integer, day_of_week text, start_time integer, end_time integer)')
+
 def initialize(cursor, document):
-    cursor.executemany('INSERT INTO employees VALUES (?, ?, ?, ?, ?, ?)', document) 
+    cursor.executemany('INSERT INTO availability VALUES (?, ?, ?, ?)', document) 
 
 def delete_record(cursor, unique_ID):
     cursor.execute("""DELETE from employees where teammember_ID = ?""", unique_ID)
@@ -42,22 +67,27 @@ def delete_record(cursor, unique_ID):
 def insert_record(cursor):
     cursor.execute('INSERT INTO employees VALUES (NULL, "do", 22.22, 0, 0, 20200304)')
 
+def drop_table(cursor, name_of_table):
+    statement = "DROP TABLE {tablename}".format(tablename = name_of_table)
+    cursor.execute(statement)
+
 def main():
     # Schema: (514926, "John Doe", 16.50, 0, 0, "20200228"), date schema: yyyymmdd
-    conn = sqlite3.connect('toy.db')
+    conn = sqlite3.connect('sof.db')
     cursor = conn.cursor()
 
-    create_table(cursor)
-    document = read_employee_records()
-    initialize(cursor, document)
-
-    # insert_record(cursor)
-    # select(cursor)
+    # document = read_availability_records()
+    # create_availability_table(cursor)
+    # initialize(cursor, document)
+    select(cursor, 'availability', 'teammember_ID = 387901')
     
     conn.commit()
     conn.close()
 
+main()
+''''
 try:
-    main()
+    
 except Exception as e:
     print(e, "Check file for missing or incorrect entries!")
+'''
